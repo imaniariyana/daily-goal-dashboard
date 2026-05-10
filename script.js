db5953ce19b225bc59402e7f1792f12bd7ef3a95
IMANI BENNETT <imanib1011@gmail.com>
2026-05-09 23:03:59 -0400

    Add goal item functionality and improve styling

--- script.js
+++ script.js
@@ -4,14 +4,33 @@ const goalList = document.querySelector(".goal-list");
 
 goalForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
+
   const goalText = goalInput.value.trim();
+
+  if (!goalText) {
+    return;
+  }
+
   const li = document.createElement("li");
   li.classList.add("goal-item");
-  li.textContent = goalText;
+
+  li.innerHTML = `
+  <div class="goal-li-content">
+    <input type="checkbox" class="goal-checkbox">
+    <span>${goalText}</span>
+    </div>
+    <div class="goal-item-btn">
+      <button type="button" class="delete-btn">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+    </div>
+  `;
+
+  const deleteBtn = li.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
   goalList.appendChild(li);
   goalForm.reset();
-
-  console.log(goalText);
-  console.log(li);
-  console.log(goalList);
 });
